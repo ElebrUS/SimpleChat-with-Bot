17f88f7dba9815ce3ad07e00e88e89410bf24309
Nikita <megaelebrus@gmail.com>
2022-08-23 23:09:55 +0300

    Update baseUrl

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://serverskyfull.herokuapp.com' }),
     tagTypes: ['Person'],
     endpoints: builder => ({
         getPerson: builder.query({
